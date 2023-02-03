2b6d65c437303425a55d4e881622eea3d08a8bcb
Maksim <polonets.maksim@gmail.com>
2023-02-03 16:59:07 +0300

    add: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,38 @@
+const GITHUBS = [
+  {
+    github: 'pavelgorbach',
+    link: 'https://github.com/pavelgorbach'
+  },
+  {
+    github: 'maxxxer',
+    link: 'https://github.com/maxxxer'
+  },
+  {
+    github: 'kirillmolotkov',
+    link: 'https://github.com/kirillmolotkov'
+  }
+]
+
 export function Footer() {
   return (
     <footer className="bg-white p-4">
       <div className="flex container m-auto justify-between">
-        <div>
+        <div className="flex">
           <a className="text-black" href="https://rs.school/js/">
-            RSSchool
+            <img className="w-20" src="src/assets/logo_rs_text.svg"></img>
           </a>
         </div>
-
         <div className="flex flex-row gap-4">
-          <div>pavelgorbach</div>
-          <div>maxxxer</div>
-          <div>kirillmolotkov</div>
+          {GITHUBS.map((github, index) => (
+            <div key={index}>
+              <a className="text-black" href={github.link}>
+                <img className="inline-block w-10 px-2" src="src/assets/github.svg"></img>
+                {github.github}
+              </a>
+            </div>
+          ))}
         </div>
-
-        <div>2022</div>
+        <div className="font-bold">2022</div>
       </div>
     </footer>
   )
