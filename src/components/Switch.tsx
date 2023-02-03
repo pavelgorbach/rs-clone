72e0b12c979ce6a81293236b982eb1590caaa22a
Pavel Gorbach <polgorbach@gmail.com>
2023-02-03 12:51:45 +0300

    refactor: Header, Button, Switch styles

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -10,7 +10,7 @@ export function Switch({ enabled, onChange }: Props) {
     <USwitch
       checked={enabled}
       onChange={onChange}
-      className={`${enabled ? 'bg-teal-900' : 'bg-teal-700'}
+      className={`${enabled ? 'bg-purple-100' : 'bg-purple-900'}
           relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
     >
       <span className="sr-only">Use setting</span>
