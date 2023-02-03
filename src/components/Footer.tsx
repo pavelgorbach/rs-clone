5e366c8db305e8f3a4dc7e90cd2ca04d22b64e91
Maksim <polonets.maksim@gmail.com>
2023-02-03 15:57:20 +0300

    init: start footer task

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 export function Footer() {
   return (
     <footer className="bg-white p-4">
-      <div className="container m-auto flex justify-between">
-        <div>RSSchool</div>
+      <div className="flex container m-auto justify-between">
+        <div>
+          <a className="text-black" href="https://rs.school/js/">
+            RSSchool
+          </a>
+        </div>
 
         <div className="flex flex-row gap-4">
           <div>pavelgorbach</div>
