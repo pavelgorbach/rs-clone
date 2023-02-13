711da4dab6f55deb6f366aa555218c33fa10f6bf
Pavel Gorbach <polgorbach@gmail.com>
2023-02-13 13:12:33 +0300

    refactor: language selector styles

--- src/components/Listbox.tsx
+++ src/components/Listbox.tsx
@@ -10,10 +10,10 @@ type Props = {
 
 export function Listbox({ value, options, onChange }: Props) {
   return (
-    <div className="w-20">
+    <div className="w-16">
       <UListbox value={value} onChange={onChange}>
         <div className="relative z-10 mt-1">
-          <UListbox.Button className="relative w-full cursor-default bg-gray-100 py-2 pl-3 pr-10 text-left shadow-sm focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <UListbox.Button className="relative w-full cursor-default bg-gray-100 py-2 pl-3 pr-5 text-left shadow-sm focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
             <span className="block truncate">{value}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -30,8 +30,8 @@ export function Listbox({ value, options, onChange }: Props) {
                 <UListbox.Option
                   key={idx}
                   className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                    `relative cursor-default select-none py-1 pl-8 pr-2 ${
+                      active ? 'bg-purple-100 text-purple-900' : 'text-gray-900'
                     }`
                   }
                   value={option}
@@ -44,7 +44,7 @@ export function Listbox({ value, options, onChange }: Props) {
                         {option}
                       </span>
                       {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-1 text-purple-600">
                           <CheckIcon className="h-5 w-5" aria-hidden="true" />
                         </span>
                       ) : null}
