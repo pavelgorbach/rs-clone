4f254f688b2586e6dcd16f1c5830123f43d0bff7
Pavel Gorbach <polgorbach@gmail.com>
2023-02-17 03:14:36 +0300

    refactor: move signIn/signUp requests to hooks

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 import { ROUTES } from '@/router'
-import { SignUpForm } from '@/components'
-import useSignUpPage from './useSignUpPage'
+import { SignUpForm } from '@/components/SignUpForm'
+import useSignUp from '@/hooks/useSignUp'
 
 export default function SignUpPageView() {
   const { t } = useTranslation()
-  const { onSubmit } = useSignUpPage()
+  const navigate = useNavigate()
+
+  const goToSignInPage = () => navigate(ROUTES.signIn, { replace: true })
+
+  const { mutate: signUp } = useSignUp(goToSignInPage)
 
   return (
     <div className="container m-auto">
       <h1 className="text-center">{t('common.welcome')}</h1>
 
       <div className="m-auto grid max-w-md bg-white p-4 shadow-md">
-        <SignUpForm onSubmit={onSubmit} />
+        <SignUpForm onSubmit={signUp} />
 
         <div className="prose-sm m-auto flex items-center gap-2">
           <p>{t('signUpForm.haveAnAccount')}</p>
