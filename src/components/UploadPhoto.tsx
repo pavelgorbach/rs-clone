50dae7036b5e08f0fb3cb0342696804a0cba611f
Maxxxer <polonets.maksim@gmail.com>
2023-02-18 22:08:07 +0300

    feat: add photo modal layout

--- src/components/UploadPhoto.tsx
+++ src/components/UploadPhoto.tsx
@@ -1,6 +1,7 @@
 import { Button } from './Button'
 import { useForm } from 'react-hook-form'
 import { FileList } from '@/api'
+import downLoadIcon from 'public/icons/download-file-round-icon.svg'
 
 type Props = {
   onSubmit: (data: FileList) => void
@@ -11,8 +12,15 @@ export function UploadPhoto({ onSubmit }: Props) {
   const submit = handleSubmit(onSubmit)
 
   return (
-    <form className="aspect-square flex flex-col">
-      <input {...register('file')} type="file" id="photo" />
+    <form className="aspect-square flex flex-col items-center">
+      <label
+        htmlFor="photo"
+        className="mb-4 flex h-80 w-full flex-col items-center justify-center gap-10 border-2 border-dashed"
+      >
+        <div>Drag&apos;n&apos;Drop your photo!</div>
+        <img className="w-1/5" src={downLoadIcon} alt="" />
+      </label>
+      <input className="absolute -z-10 opacity-0" {...register('file')} type="file" id="photo" />
       <Button text={'Upload'} onClick={submit}></Button>
     </form>
   )
