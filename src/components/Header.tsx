40c3ca778d052ace8ca096a3ac088c3380c7c936
Pavel Gorbach <polgorbach@gmail.com>
2023-02-18 10:25:37 +0300

    refactor: move theme logic to mobx store

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,46 @@
-import { Link } from 'react-router-dom'
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Link, useNavigate } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
 import { Tooltip } from 'react-tooltip'
-import {
-  ViewColumnsIcon,
-  ArrowRightOnRectangleIcon,
-  HomeIcon,
-  UserIcon
-} from '@heroicons/react/24/solid'
+import { useTranslation } from 'react-i18next'
 
-import { ROUTES } from '@/router'
-import { Button, Switch, Listbox } from '@/components'
-import useHeader from './useHeader'
+import ViewColumnsIcon from '@heroicons/react/24/solid/ViewColumnsIcon'
+import ArrowRightOnRectangleIcon from '@heroicons/react/24/solid/ArrowRightOnRectangleIcon'
+import HomeIcon from '@heroicons/react/24/solid/HomeIcon'
+import UserIcon from '@heroicons/react/24/solid/UserIcon'
+
+import { ROUTES } from '@/router/routes'
+import { Button, ThemeSwitch, Listbox } from '@/components'
+import useAuthStore from '@/hooks/useAuthStore'
+import useModalStore from '@/hooks/useModalStore'
 
 function HeaderView() {
-  const {
-    i18n,
-    isAuthenticated,
-    theme,
-    t,
-    toggleTheme,
-    changeLanguage,
-    goToSignInPage,
-    goToSignUpPage,
-    onAddBoard,
-    onSignOut
-  } = useHeader()
+  const { t, i18n } = useTranslation()
+  const navigate = useNavigate()
+
+  const authStore = useAuthStore()
+  const modal = useModalStore()
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng)
+  }
+
+  const onAddBoard = () => {
+    navigate(ROUTES.boards)
+    modal.open({ name: 'add-board', data: { userId: authStore.userId! } })
+  }
+
+  const onSignOut = () => {
+    authStore.unauth()
+  }
+
+  const goToSignInPage = () => {
+    navigate(ROUTES.signIn)
+  }
+
+  const goToSignUpPage = () => {
+    navigate(ROUTES.signUp)
+  }
 
   return (
     <header className="bg-white py-2 px-4 dark:bg-slate-800">
@@ -35,18 +51,18 @@ function HeaderView() {
           </h1>
         </Link>
 
-        <Switch enabled={theme} onClick={toggleTheme} />
+        <ThemeSwitch />
 
         <Listbox value={i18n.language} options={['ru', 'en']} onChange={changeLanguage} />
 
-        {!isAuthenticated && (
+        {!authStore.isAuthenticated && (
           <div className="ml-auto flex items-center gap-4">
             <Button text={t('common.signIn')} onClick={goToSignInPage} />
             <Button text={t('common.signUp')} onClick={goToSignUpPage} />
           </div>
         )}
 
-        {isAuthenticated && (
+        {authStore.isAuthenticated && (
           <div className="ml-auto flex items-center gap-4">
             <Button className="hidden md:block" text={t('header.addBoard')} onClick={onAddBoard} />
 
--- src/hooks/useAuthStore.ts
+++ src/hooks/useAuthStore.ts
@@ -2,6 +2,6 @@ import { useContext } from 'react'
 import { StoreContext } from '@/store.context'
 
 export default function useAuthStore() {
-  const { authStore } = useContext(StoreContext)
-  return authStore
+  const store = useContext(StoreContext)
+  return store.authStore
 }
