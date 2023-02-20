8f74c14ae998fd46f7581aac8a1180d7bcfa0780
Pavel Gorbach <polgorbach@gmail.com>
2023-02-20 10:52:04 +0300

    refactor: store theme to localstorage

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,20 @@
 import { observer } from 'mobx-react-lite'
 import { Outlet } from 'react-router'
 import { ToastContainer } from 'react-toastify'
-import cx from 'classnames'
 
 import { Header, Footer, Modals } from '@/components'
-import useThemeStore from '@/hooks/useThemeStore'
 
 export function LayoutView() {
-  const theme = useThemeStore()
-
   return (
-    <div className={cx(theme.theme)}>
-      <div className="flex min-h-screen flex-col bg-gray-50 dark:bg-slate-700">
-        <Header />
-        <main className="xl:prose-md prose-sm flex flex-1 flex-col p-4 lg:prose-base 2xl:prose-lg">
-          <Outlet />
-        </main>
-        <Footer />
+    <div className="flex min-h-screen flex-col bg-gray-50 dark:bg-slate-700">
+      <Header />
+      <main className="xl:prose-md prose-sm flex flex-1 flex-col p-4 lg:prose-base 2xl:prose-lg">
+        <Outlet />
+      </main>
+      <Footer />
 
-        <Modals />
-        <ToastContainer />
-      </div>
+      <Modals />
+      <ToastContainer />
     </div>
   )
 }
